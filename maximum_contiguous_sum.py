85f9d46989db0b489eddf3deb742d06df896a8c1
Aditya Samalla <aditya.samalla@asu.edu>
2018-11-19 10:22:28 -0800

    Update maximum_contiguous_sum.py

--- maximum_contiguous_sum.py
+++ maximum_contiguous_sum.py
@@ -4,7 +4,7 @@ def maximum_sum(arr):
     n=len(arr)-1
     while(i<=n):
         cursum += arr[i]
-        cursum = max(0,cursum)
+        cursum = max(i,cursum)
         maxsum = max(cursum,maxsum)
         i+=1
     return(maxsum)
