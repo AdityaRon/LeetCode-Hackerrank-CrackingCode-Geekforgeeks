9fb71327238730a416af20eb19abdb9ea21d3164
Aditya Samalla <aditya.samalla@asu.edu>
2020-10-20 00:04:26 -0400

    Update most_common_word.py

--- most_common_word.py
+++ most_common_word.py
@@ -10,7 +10,11 @@ class Solution:
         for i in sorted(frequency, key=frequency.get, reverse=True):
             if i not in banned:
                 return i
-            
-        
+           
+    #alternative solution    
+    def mostCommonWord(self, p, banned):
+        ban = set(banned)
+        words = re.findall(r'\w+', p.lower())
+        return collections.Counter(w for w in words if w not in ban).most_common(1)[0][0]
             
         
