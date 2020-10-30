1198fb8e9a1482ef706eb95f0f6a13b5db9dafe5
Aditya Samalla <aditya.samalla@asu.edu>
2020-10-30 00:15:01 -0400

    Update long_pressed_name.py

--- long_pressed_name.py
+++ long_pressed_name.py
@@ -1,12 +1,24 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
-        from collections import Counter
         self.name = name
         self.typed = typed
+        # i = 0
+        # for j in range(len(typed)):
+        #     if i < len(name) and typed[j] == name[i]:
+        #         i+=1
+        #     elif j == 0 or typed[j] != typed[j-1]:
+        #         return False
+        # return i == len(name)
+    
         i = 0
-        for j in range(len(typed)):
-            if i < len(name) and typed[j] == name[i]:
+        j = 0
+        while (i <= len(typed)-1):
+            if j <= len(name)-1 and typed[i] == name[j]:
                 i+=1
-            elif j == 0 or typed[j] != typed[j-1]:
+                j+=1
+            elif j == 0 or typed[i] != typed[i-1]:
                 return False
-        return i == len(name)
+            else:
+                i+=1
+        return j == len(name)
+            
