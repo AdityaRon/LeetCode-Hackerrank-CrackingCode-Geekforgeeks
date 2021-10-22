44b6c89b76a0c1cab70ca110e994cc5e7a1b40b1
Aditya Samalla <aditya.samalla@asu.edu>
2021-10-22 16:21:27 -0700

    Update largest_substring_between_two_equal_characters.py

--- largest_substring_between_two_equal_characters.py
+++ largest_substring_between_two_equal_characters.py
@@ -15,3 +15,23 @@ class Solution:
                     else:
                         count +=1
             return maxcount
+
+#approach2
+
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        chars = {}
+        maximum = 0
+        if len(s) == len(set(s)):
+            return -1
+        else:
+            for i,j in enumerate(s):
+                if j not in chars:
+                    chars[j] = [i]
+                else:
+                    chars[j].append(i)
+            for key, value in chars.items():
+                if (max(value)-min(value)-1) > maximum:
+                    maximum = max(value)-min(value)-1
+            return maximum
+                    
