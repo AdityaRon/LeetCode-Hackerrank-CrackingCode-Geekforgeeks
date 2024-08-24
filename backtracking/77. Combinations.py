e0613fe85a0be961d343b42df53381f2569b9a8f
Aditya Samalla <aditya.samalla@asu.edu>
2024-08-24 15:13:31 -0700

    Create 77. Combinations.py

---
+++ backtracking/77. Combinations.py
@@ -0,0 +1,48 @@
+
+# Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+
+# You may return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: n = 4, k = 2
+# Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+# Explanation: There are 4 choose 2 = 6 total combinations.
+# Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+# Example 2:
+
+# Input: n = 1, k = 1
+# Output: [[1]]
+# Explanation: There is 1 choose 1 = 1 total combination.
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result, solution = [], []
+        # method 1 recursion
+        # def combinations(x):
+        #     if len(solution) == k:
+        #         result.append(solution.copy())
+        #         return
+        #     still_needed = k - len(solution)
+        #     left = x
+        #     if left > still_needed:
+        #         combinations(x-1)
+        #     solution.append(x)
+        #     combinations(x-1)
+        #     solution.pop()
+        # combinations(n)
+        # return result
+
+        #method 2:
+
+        def combinations(x, comb):
+            if len(comb) == k:
+                result.append(comb.copy())
+            for i in range(x, n+1):
+                comb.append(i)
+                combinations(i+1, comb)
+                comb.pop()
+        combinations(1, solution)
+        return result
