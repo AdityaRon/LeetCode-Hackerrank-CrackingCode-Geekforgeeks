d42a78ccddaf6d4387f3e0ae9883989ec100a400
Aditya Samalla <aditya.samalla@asu.edu>
2024-11-09 19:39:22 -0800

    Create level_order_traversal.py

---
+++ graphs & trees/level_order_traversal.py
@@ -0,0 +1,41 @@
+"""
+102. Binary Tree Level Order Traversal
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+
+        if not root:
+            return []
+
+        from collections import deque
+        queue = deque()        
+        queue.append(root)
+        ans = []
+
+        while queue:
+            level = []
+            n = len(queue)
+            for i in range(n):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            ans.append(level)
+        return ans
+
+
+        
